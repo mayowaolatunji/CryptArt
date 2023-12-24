b063c3ce6b28c400029c4e935afaaa62d08aaf6d
mayowaolatunji <olatunjimayowa0396@gmail.com>
2023-12-24 22:46:49 +0100

    Footer/ CryptArt

--- cryptArt/src/components/footer/footer.jsx
+++ cryptArt/src/components/footer/footer.jsx
@@ -5,7 +5,51 @@ import './footer.css';
 
 function footer() {
   return (
-    <div className='Footer'><h1>Footer</h1></div>
+    <div className='Footer'>
+
+      <div className="footerContent">
+
+      <div className="list">
+        <ul>
+          <li className='headerText'>Account</li>
+          <li>Profile</li>
+          <li>Studio</li>
+          <li>mints</li>
+        </ul>
+      </div>
+
+      <div className="list">
+        <ul>
+          <li className='headerText' >Resource</li>
+          <li>Documentation</li>
+          <li>Developer</li>
+          <li>Learn</li>
+          <li>Help</li>
+        </ul>
+
+      </div>
+
+      <div className="list">
+      <ul>
+          <li className='headerText'>Stats</li>
+          <li>Rankings</li>
+          <li>Collections</li>
+          <li>Art status</li>
+        </ul>
+      </div>
+
+      <div className="community">
+        <ul>
+          <li className='headerText'>Community</li>
+          <li>dhdj</li>
+          <li>dhdj</li>
+          <li>dhdj</li>
+        </ul>
+      </div>
+
+      </div>
+
+    </div>
   )
 }
 
