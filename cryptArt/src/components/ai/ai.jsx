422da5e7a8e0f97f825ea2b6386c9a20b8a740c9
mayowaolatunji <olatunjimayowa0396@gmail.com>
2023-12-26 17:13:18 +0100

    cryptArt AI sibar completed.

--- cryptArt/src/components/ai/ai.jsx
+++ cryptArt/src/components/ai/ai.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 import './ai.css';
 
+import Arrow from './arrow.svg'
+
 
 function ai() {
   return (
     <div className='aiContainer'>
-      <h1>Ai</h1>
+
+      <div className="sideCard">
+       <div className='text'>
+         <h1>Create Custom NFTs <br /> With AI</h1>
+         <p>Bring Your Imaginations to Life... <br /> Powered by  <span>cryptArt MLL.</span></p>
+       </div>
+       <div className='btn'>
+          <button><p>Create NFTs</p></button>
+          <img src={Arrow} alt="" />
+       </div>
+      </div>
+
+      <div className="aiCard"></div>
     
     </div>
   )
