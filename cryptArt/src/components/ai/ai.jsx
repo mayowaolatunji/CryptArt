2df58bc2781de55a8aa9f315430d509282197436
mayowaolatunji <olatunjimayowa0396@gmail.com>
2023-12-28 13:48:17 +0100

    AI text - image generator section completed with OpenAi API...

--- cryptArt/src/components/ai/ai.jsx
+++ cryptArt/src/components/ai/ai.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import './ai.css';
 
@@ -8,15 +8,37 @@ import DefaultImg from './default_img.png'
 
 function ai() {
 
-  const [image_url, setImage_url] useState("/");
+  // API key = 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
 
-  let inputRef = useRef(nul);
+  const [image_url, setImage_url] = useState("/");
+
+  let inputRef = useRef(null)
 
   const imageGenerator = async ()=>{
     if (inputRef.current.value ===""){
       return 0;
     }
-    const response = await fetch()
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers:{
+         "Content-Type": "application/json",
+         Authorization: 
+         "Bearer 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",
+         "User-Agent" : "Chrome", 
+        },
+        body : JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n :1,
+          size: "400X400"
+        }),
+      }
+    );
+    let data = await response.json();
+    console.log(data);
+    let data_array = data.data;
+    setImage_url(data_array[0].url);
   }
   return (
     <div className='aiContainer'>
@@ -39,6 +61,7 @@ function ai() {
 
         <div className="searchBox">
           <input type="text" ref={inputRef} placeholder='Describe your Art'/>
+          <button className='generate-btn' onClick={()=>{imageGenerator()}}>Generate</button>
         </div>
       </div>
     
